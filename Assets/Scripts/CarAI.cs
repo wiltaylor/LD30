83266a3eae63e95d0a6adc57ec1331a40bd80b15
Wil Taylor <wilfridtaylor@gmail.com>
2014-08-23 16:53:17 +1000

    Added basic game AI, can do laps of track

---
+++ Assets/Scripts/CarAI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarAI : MonoBehaviour
+{
+    public GameObject NavNode;
+    public float MaxDistanceToNode = 5f;
+    public float Speed = 1f;
+
+    private Rigidbody2D rbody;
+	void Start ()
+	{
+	    rbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+	{
+	    var dist = Vector3.Distance(NavNode.transform.position, transform.position);
+
+	    if (dist < MaxDistanceToNode)
+	    {
+	        NavNode = NavNode.GetComponent<NavNodeController>().NextNode;
+            dist = Vector3.Distance(NavNode.transform.position, transform.position);
+	    }
+
+
+
+	    rbody.velocity = (NavNode.transform.position - transform.position).normalized*Speed;
+
+
+	}
+}
