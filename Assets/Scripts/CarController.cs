13a23448d159b671de533f73f89b9ac9eaa2d64e
Wil Taylor <wilfridtaylor@gmail.com>
2014-08-23 15:59:43 +1000

    Setup basic player controls and a basic test level.

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+
+    private Rigidbody2D _rbody;
+    public float EnginePower = 1f;
+    public float BrakePower = 0.5f;
+    public float TurnSpeed = 10f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rbody = gameObject.rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetAxis("Vertical") > 0f)
+	    {
+            _rbody.AddForce(transform.up.normalized * EnginePower, ForceMode2D.Impulse);
+	    }
+
+        if (Input.GetAxis("Vertical") < 0f)
+        {
+            _rbody.AddForce(-transform.up.normalized * BrakePower, ForceMode2D.Impulse);
+        }
+
+	    if (Input.GetAxis("Horizontal") < 0f)
+	    {
+            _rbody.MoveRotation(_rbody.rotation + TurnSpeed * Time.deltaTime);
+	    }
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            _rbody.MoveRotation(_rbody.rotation - TurnSpeed * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Scripts/SlowZone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowZone : MonoBehaviour
+{
+    public float Drag = 10f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var car = other.GetComponent<CarController>();
+
+        if (car == null)
+            return;
+
+        var body = other.GetComponent<Rigidbody2D>();
+
+        body.drag += Drag;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        var car = other.GetComponent<CarController>();
+
+        if (car == null)
+            return;
+
+        var body = other.GetComponent<Rigidbody2D>();
+
+        body.drag -= Drag;
+    }
+}
