893f6066185fc96933e5fab2e58dff7dd1568dbe
Wil Taylor <wilfridtaylor@gmail.com>
2014-08-24 14:59:21 +1000

    Add music, fixed car animations and started work on main menu,

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public Texture2D Logo;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 512, 128), Logo);
+    }
+}
